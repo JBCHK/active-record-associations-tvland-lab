34a5c31dc8e6749762dc3dea81839e93187c1e39
Jonathan Block <blockjonathan29@gmail.com>
2021-04-12 17:02:16 -0400

    commit tree f6e4c77a2e8ccc2c0000f8e46db00b893ceda743

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,13 @@
 class Show < ActiveRecord::Base
-  
+  belongs_to :network
+  has_many :characters
+  has_many :actors, through: :characters
+
+  def actors_list
+    self.characters.collect do |c|
+      list = c.actor.full_name
+      list
+    end
+  end
+
 end
